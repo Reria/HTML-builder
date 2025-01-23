9f10d5bebfc4876a4f453262ddd72cad2dd8294e
Reria <42136407+Reria@users.noreply.github.com>
2025-01-23 12:34:07 +0300

    fix: fixed task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,38 +1,26 @@
-
-const fs = require('fs').promises;
+const fs = require('fs/promises');
 const path = require('path');
-
-// Функция для копирования директории
-async function copyDir(source, destination) {
+async function copyDir() {
+  const dirPath = path.join(__dirname, 'files');
+  const newFolder = path.join(__dirname, 'files-copy');
   try {
-    //  Создаем папку `files-copy`, если ее еще не существует.
-    await fs.mkdir(destination, { recursive: true });
-
-    // Читаем содержимое папки `files`.
-    const files = await fs.readdir(source);
-
-    // Копируем файлы из папки `files` в папку `files-copy`.
-    for (const file of files) {
-      const sourcePath = path.join(source, file);
-      const destinationPath = path.join(destination, file);
-
-      // Используем fs.stat, чтобы проверить, является ли текущий элемент файлом или директорией.
-      const stats = await fs.stat(sourcePath);
-
-      if (stats.isFile()) {
-        // Если это файл, копируем его в папку назначения.
-        await fs.copyFile(sourcePath, destinationPath);
-      } else if (stats.isDirectory()) {
-        // Если это директория, рекурсивно вызываем copyDir.
-        await copyDir(sourcePath, destinationPath);
+    await fs.mkdir(newFolder, { recursive: true });
+    const allFiles = await fs.readdir(dirPath, { withFileTypes: true });
+    const deleteFiles = await fs.readdir(newFolder, { withFileTypes: true });
+    for (const fileD of deleteFiles) {
+      const filePath = path.join(newFolder, fileD.name);
+      await fs.rm(filePath, { recursive: true, force: true });
+    }
+    for (const file of allFiles) {
+      const dirFilePath = path.join(dirPath, file.name);
+      const newFilePath = path.join(newFolder, file.name);
+      if (file.isFile()) {
+        await fs.copyFile(dirFilePath, newFilePath);
       }
     }
-
-    console.log('Директория успешно скопирована!');
-  } catch (error) {
-    console.error('Ошибка при копировании директории:', error.message);
+    console.log('Copyright done!');
+  } catch (err) {
+    console.log('Error to copyright!', err.message);
   }
 }
-
-// Вызываем функцию copyDir с указанием исходной и целевой директорий.
-copyDir('files', 'files-copy');
+copyDir();
